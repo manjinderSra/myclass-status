c2d5ef98ccd9ef4e91d3500530a7f87ab6265454
manjinder-singh-sra <manjinder.bmdu@gmail.com>
2026-08-30 14:23:33 +0530

    Test PHP runtime

--- api/index.php
+++ api/index.php
@@ -1,15 +1,9 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+header('Content-Type: application/json');
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$request = Illuminate\Http\Request::capture();
-
-$response = $kernel->handle($request);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+echo json_encode([
+    'status' => 'PHP WORKING',
+    'php_version' => PHP_VERSION,
+    'time' => date('Y-m-d H:i:s')
+]);
